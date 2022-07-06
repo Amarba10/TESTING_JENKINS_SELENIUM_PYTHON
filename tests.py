6ed4f03d604b6b9e3696127ee0f90bc9fc24e8ec
amar <barake.amar@gmail.com>
2022-07-06 12:08:49 +0300

    Second Test Text

--- tests.py
+++ tests.py
@@ -5,3 +5,8 @@ def test_base_route_hello_world():
     response = requests.get("http://localhost:3000/")
     assert response.status_code == 200
 
+
+def test_text():
+    response = requests.get("http://localhost:3000/")
+    assert response.text == 'Welcome to the Application with updated code!'
+
